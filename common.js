004d9c27f6d3a52c9540b1d967b7d69bc5b1a7ac
moum-Development <154895390+moum-Development@users.noreply.github.com>
2024-02-21 18:58:41 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: 'https://daemyungsangjo.kr/ip.php',
+        url: 'http://daemyungsangjo.kr/ip.php', // HTTP 프로토콜 사용
         type: 'GET',
         dataType: 'text',
         success: function(response) {
